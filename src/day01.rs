6aa420929a9057e1ae893e1e1a29d1bcb6f578ed
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-12-01 19:31:42 +0100

    Finished with day 1 :D

--- src/day01.rs
+++ src/day01.rs
@@ -1,8 +1,21 @@
 use utils::parse;
+use hashbrown::HashSet;
 
 pub fn run() -> Result<i32, Box<std::error::Error>> {
-    let out = parse("src/inputs/day01.txt")?;
-    let sum: i32 = out.iter().sum();
+    let out = parse("inputs/day01.txt")?;
+    let mut frequencies = HashSet::new();
+    let mut current_frequency = 0;
+    let mut curr_pos = 0;
+    // Answer to part one
+    // let sum: i32 = out.iter().sum();
 
-    Ok(sum)
+    frequencies.insert(0);
+    loop {
+        current_frequency += out[curr_pos % out.len()];
+        curr_pos += 1;
+        if frequencies.contains(&current_frequency) {
+            return Ok(current_frequency);
+        }
+        frequencies.insert(current_frequency);
+    }
 }
